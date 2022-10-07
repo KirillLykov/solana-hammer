e9933bb446c9b356c1e1b79c6a2c71751ebb9a12
Kirill Lykov <kirill.lykov@solana.com>
2022-10-07 09:27:57 +0000

    modified script

--- launch_with_test_validator.sh
+++ launch_with_test_validator.sh
@@ -13,12 +13,13 @@
 #solana-keygen new -o keys/admin.json
 #solana airdrop 50000 <PUBKEY> 
 #for i in `seq 1 10`; do solana -u localhost --use-quic -k keys/admin.json program deploy --program-id keys/program_$i.json target/program_$i.so; done
-# cargo build --manifest-path client/Cargo.toml --release
+#cargo build --manifest-path client/Cargo.toml --release
 #for i in `seq 1 10`; do ./client/target/release/make_contention_accounts --fee-payer ./keys/admin.json --rpc-node http://localhost:8899  --program-id ./keys/program_$i.json; done
 
 #./client/target/release/fetch_tpu --gossip-entrypoint 127.0.0.1:1024 --output-file tpu.bin
 
 cargo build --manifest-path client/Cargo.toml --release
+
 ./client/target/release/client\
          --tpu-file tpu.bin \
          --rpc-server http://localhost:8899 \
@@ -26,7 +27,13 @@ cargo build --manifest-path client/Cargo.toml --release
          --program-id  59TbbR1hF3hZbeeV7PZTPp9zGaVybETevnTe6S2qJywK\
          --program-id  49Ffhy9USyAFKxEn1NLy9wBcJg2yoZLFkpaUWn71NmpH\
          --program-id  6uPMsjKkjrKwB63rMVuuCdPAewFzsqyepbkP8xCMJe2z\
+         --program-id  ArNBtqVzEouodAjhMyhAZTLQN3WGSxYWphaAhHGZTyA8\
+         --program-id  FFQS133TdW45GEENfdB4FDY6dDz3qGAJ3icMpz2wrTXF\
+         --program-id  3J5KaeMxRnbmEcRiocJQajgF7sdavTVjSHELnruf5XGG\
+         --program-id  G9vuyqzH7QvGjjwxjE3rgFWEP8fyqiw8hG5asu53L3NZ\
+         --program-id  FsEzgc47HApzrJUdhFtLMKjypc3K3yippQ2wi7X99zYs\
+         --program-id  FA9f5Vazh5BbjiXqoZfb3G95XPDt1xwrQwi47PyS7Pjj\
+         --program-id  7azfsN2eZmQoou3AfkBaWhv2ho5M4UMmYT3N345K141W\
          --num-threads 1 \
          --total-transactions 10
-
 
